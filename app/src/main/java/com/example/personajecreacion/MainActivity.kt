c449bd80350c7bf111d490d9252a65fd7d605648
CarmenPalomo <carpal162@alu.centroafuera.es>
2023-12-16 13:18:45 +0100

    Correccion de errores en el ActivityMain y en el xml

--- app/src/main/java/com/example/personajecreacion/MainActivity.kt
+++ app/src/main/java/com/example/personajecreacion/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.personajecreacion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 
@@ -12,25 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val boton : Button = findViewById(R.id.button)
-        //prueba error git
-        val botonRaza : Spinner = findViewById(R.id.spinner1)
-        val botonClase : Spinner = findViewById(R.id.spinner2)
-        val botonEstado : Spinner = findViewById(R.id.spinner3)
-
-
-        ArrayAdapter.createFromResource(this, R.array.raza, android.R.layout.simple_spinner_item).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            botonRaza.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(this, R.array.clase, android.R.layout.simple_spinner_item).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            botonRaza.adapter = adapter
-        }
-        ArrayAdapter.createFromResource(this, R.array.EstadoVital, android.R.layout.simple_spinner_item).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            botonRaza.adapter = adapter
-        }
+        val boton : Button = findViewById(R.id.boton_siguiente)
+        val spinnerRaza : Spinner = findViewById(R.id.spinner_raza)
+        val spinnerClase : Spinner = findViewById(R.id.spinner_clase)
+        val spinnerEstado : Spinner = findViewById(R.id.spinner_edad)
 
 
         boton.setOnClickListener {
